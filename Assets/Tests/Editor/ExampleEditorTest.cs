a8eed9e98a328d06e658351bdf3e682767c22aee
Astrid Avalin Soerensen <Avalin@users.noreply.github.com>
2025-03-30 17:43:00 +0200

    Added tests for PlayMode and EditMode as concepts

---
+++ Assets/Tests/Editor/ExampleEditorTest.cs
@@ -0,0 +1,11 @@
+using NUnit.Framework;
+
+public class ExampleEditorTest
+{
+    [Test]
+    public void ThisTestWillPass()
+    {
+        Assert.AreEqual(1 + 1, 2);
+    }
+}
+
---
+++ Assets/Tests/PlayMode/ExamplePlayModeTest.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+public class ExamplePlayModeTest
+{
+    [UnityTest]
+    public IEnumerator ThisTestAlsoPasses()
+    {
+        yield return null;
+        Assert.IsTrue(true);
+    }
+}
