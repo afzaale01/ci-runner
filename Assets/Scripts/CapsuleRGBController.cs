b6086c601741aec71e333b25931f4c51d570a836
Astrid Avalin Soerensen <Avalin@users.noreply.github.com>
2025-04-15 15:32:28 +0200

    Added Unity Scene changes
    Update SampleScene.unity
    
    Added some interaction for the lulz

---
+++ Assets/Scripts/CapsuleRGBController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class CapsuleRGBController : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer capsule;
+    [SerializeField] private float speed = 0.2f;
+
+    private Material capsuleMaterial;
+    private float hue;
+
+    private void Start()
+    {
+        capsuleMaterial = capsule.material;
+        hue = 0f;
+    }
+
+    private void Update()
+    {
+        hue += Time.deltaTime * speed;
+        if (hue > 1f) hue -= 1f;
+
+        Color rgbColor = Color.HSVToRGB(hue, 1f, 1f);
+        capsuleMaterial.color = rgbColor;
+    }
+}
