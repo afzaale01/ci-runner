19ca4d867a352fd92b3e5f56a35a3c2f00c5e371
Astrid Avalin Soerensen <Avalin@users.noreply.github.com>
2025-04-22 00:14:27 +0200

    Revert "ROSLYN-AUTOFIX: auto-format C# files with CSharpier"
    This reverts commit a584a3efda0423227b579636978e542091ff56a3.

--- Assets/Tests/Editor/ExampleEditorTest.cs
+++ Assets/Tests/Editor/ExampleEditorTest.cs
@@ -1,7 +1,9 @@
 using NUnit.Framework;
 
+public class ExampleEditorTest
 {
     [Test]
+    public void ThisTestWillPass()
     {
         Assert.AreEqual(1 + 1, 2);
     }
--- Assets/Tests/PlayMode/ExamplePlayModeTest.cs
+++ Assets/Tests/PlayMode/ExamplePlayModeTest.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using NUnit.Framework;
 using UnityEngine.TestTools;
 
+public class ExamplePlayModeTest
 {
     [UnityTest]
+    public IEnumerator ThisTestAlsoPasses()
     {
         yield return null;
         Assert.IsTrue(true);
