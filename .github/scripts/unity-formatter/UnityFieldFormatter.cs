d987cd95a5fd65a530af3c1ab9c2132f7214e0e3
Astrid Avalin Soerensen <Avalin@users.noreply.github.com>
2025-04-22 14:09:07 +0200

    Kept build in singulr folder

--- .github/scripts/unity-formatter/UnityFieldFormatter.cs
+++ .github/scripts/unity-formatter/UnityFieldFormatter.cs
@@ -1,7 +1,6 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis;
 using UnityReorder.Extensions;
 
 namespace UnityReorder
